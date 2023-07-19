75a1b1f213a8ef112c73384da48669c0454191cb
Zabair Ghoss <czubair307@gmail.com>
2023-07-19 11:39:39 +0500

    App structure and tailwind configuration set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`max-w-[1440px] mx-auto ${inter.className}`}>{children}</body>
     </html>
   )
 }
