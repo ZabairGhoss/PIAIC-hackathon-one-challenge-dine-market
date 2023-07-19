d49448eedf69679461d63f0e662bace5f80b7029
Zabair Ghoss <czubair307@gmail.com>
2023-07-19 18:22:52 +0500

    section/Promotion completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/views/Hero'
+import Promotion from '@/views/Promotion'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Promotion />
     </main>
   )
 }
