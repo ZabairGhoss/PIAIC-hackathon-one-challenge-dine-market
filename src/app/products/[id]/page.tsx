c747809be9e66136841c011417955b8e611e4521
Zabair Ghoss <czubair307@gmail.com>
2023-07-27 23:52:51 +0500

    Product details page continued

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -13,13 +13,21 @@ const page = ({ params }: { params: { id: number } }) => {
      {
         result.map((product) => {
           return (
-            <div key={product.id} className="flex justify-between">
+            <div key={product.id} className="flex justify-between gap-6">
                 <div>
                   <Image src={product.img} alt={product.title} />
                 </div>
                 <div>
-                  <p>{product.title}</p>
-                  <p>{product.tagline}</p>
+                <div>
+                  <h1 className="text-2xl">{product.title}</h1>
+                  <h2 className="text-base font-semibold text-gray-400">{product.tagline}</h2>
+                </div>
+                <div>
+                  <h3 className="uppercase text-xs font-semibold mt-6">select size</h3>
+                  <div className="w-6 h-6 rounded-full hover:shadow-xl mt-2 text-center">
+                      <span className="uppercase text-xs font-semibold text-gray-600">xs</span>
+                  </div>
+                </div>
                 </div>
             </div>
           );
